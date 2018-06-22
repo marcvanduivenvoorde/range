3d4b258d78449cff7b8e92c75f8b58ae4e88dd00
marcvanduivenvoorde <marc.vanduivenvoorde@synetic.nl>
2018-06-22 08:31:18 +0200

    - add range object

---
+++ src/MarcVanDuivenvoorde/Range/Range.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace MarcVanDuivenvoorde\Range;
+
+use Iterator;
+
+class Range implements Iterator {
+  protected $index = 0;
+
+  /**
+   * The range.
+   *
+   * @var array
+   */
+  protected $range;
+
+  public function __construct(int $start, int $end, int $step = 1) {
+    $this->range = range($start, $end, $step);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function current() {
+    return $this->range($this->index);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function next() {
+    return $this->index++;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function key() {
+    return $this->index;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function valid() {
+    return isset($this->range[$this->index]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function rewind() {
+    $this->index = 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function toArray() {
+    return $this->range;
+  }
+
+}
